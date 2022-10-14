30fe5afaa33c3c98c418d8061e4c40aa22d129da
NaViD- <cutoni8@gmail.com>
2022-10-14 23:17:40 +0330

    first router with validation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUSerDto } from './dtos/create-user.dto';
+@Controller('auth')
+export class UsersController {
+	@Post('/signup')
+	createUser(@Body() body: CreateUSerDto) {
+		console.log(body);
 
-@Controller('users')
-export class UsersController {}
+	}
+
+}

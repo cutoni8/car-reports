6f7cc81cc81f0bb60a14c97efea1b8b0ce16bad4
NaViD- <cutoni8@gmail.com>
2022-10-24 22:33:41 +0330

    implement create report

--- src/reports/report.entity.ts
+++ src/reports/report.entity.ts
@@ -1,7 +1,32 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { User } from 'src/users/user.entity';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
 
 @Entity()
 export class Report {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
+  price: number;
+
+  @Column()
+  make: string;
+
+  @Column()
+  model: string;
+
+  @Column()
+  year: number;
+
+  @Column()
+  lng: number;
+
+  @Column()
+  lat: number;
+
+  @Column()
+  mileage: number;
+
+  @ManyToOne(() => User, (user) => user.reports)
+  user: User;
 }
